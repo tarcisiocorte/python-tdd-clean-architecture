6cba1356bc65f2ce10f734daa9cdb085be5bf5ed
Tarcisio Costa <tarcisiocorte@gmail.com>
2021-03-11 02:01:49 -0300

    refactor: add makeSut

--- test/test_signUp.py
+++ test/test_signUp.py
@@ -4,66 +4,71 @@ import json
 from src.presentation.controllers.SignUpController import SignUpController
 
 
+def makeSut():
+    sut = SignUpController()
+    return sut
+
+
 def test_should_return_400_if_no_name_is_provide():
-    signUpController = SignUpController()
+    sut = makeSut()
     body = {
         "email": "any_email@mail.com",
         "email_confirmation": "any_email@mail.com",
         "password": "any_password",
         "password_confirmation": "any_password"
     }
-    result = signUpController.handle(body)
+    result = sut.handle(body)
     assert result['status_code'] == 400
     assert result['message'] == f"Missing param: 'name'"
 
 
 def test_should_return_400_if_no_email_is_provide():
-    signUpController = SignUpController()
+    sut = makeSut()
     body = {
         "name": "any_name",
         "email_confirmation": "any_email@mail.com",
         "password": "any_password",
         "password_confirmation": "any_password"
     }
-    result = signUpController.handle(body)
+    result = sut.handle(body)
     assert result['status_code'] == 400
     assert result['message'] == f"Missing param: 'email'"
 
 
 def test_should_return_400_if_no_email_confirmation_is_provide():
-    signUpController = SignUpController()
+    sut = makeSut()
     body = {
         "name": "any_name",
         "email": "any_email@mail.com",
         "password": "any_password",
         "password_confirmation": "any_password"
     }
-    result = signUpController.handle(body)
+    result = sut.handle(body)
     assert result['status_code'] == 400
     assert result['message'] == f"Missing param: 'email_confirmation'"
 
 
 def test_should_return_400_if_no_password_is_provide():
-    signUpController = SignUpController()
+    sut = makeSut()
     body = {
         "name": "any_name",
         "email": "any_email@mail.com",
         "email_confirmation": "any_email@mail.com",
         "password_confirmation": "any_password"
     }
-    result = signUpController.handle(body)
+    result = sut.handle(body)
     assert result['status_code'] == 400
     assert result['message'] == f"Missing param: 'password'"
 
 
 def test_should_return_400_if_no_password_confirmation_is_provide():
-    signUpController = SignUpController()
+    sut = makeSut()
     body = {
         "name": "any_name",
         "email": "any_email@mail.com",
         "email_confirmation": "any_email@mail.com",
         "password": "any_password"
     }
-    result = signUpController.handle(body)
+    result = sut.handle(body)
     assert result['status_code'] == 400
     assert result['message'] == f"Missing param: 'password_confirmation'"
