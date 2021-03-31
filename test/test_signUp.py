ab14d0096959e84c97f4dd99d7c2f78ef5a1de13
Tarcisio Costa <tarcisiocorte@gmail.com>
2021-03-31 09:21:32 -0300

    test: ensure return status_code 400 if login is not provided

--- test/test_signUp.py
+++ test/test_signUp.py
@@ -14,16 +14,29 @@ def makeSut():
     return sut
 
 
-def test_should_return_400_if_no_name_is_provide():
+def test_should_return_400_if_no_login_is_provide():
     sut = SignUpController()
     attributes = {
         "name": faker.word(),
-        "specie": "Dog",
-        "age": faker.random_number(),
-        "user_information": {
-            "user_id": faker.random_number(),
-            "user_name": faker.word(),
-        },
+        "email": "any_email@mail.com",
+        "email_confirmation": "any_email@mail.com",
+        "password": "any_password",
+        "password_confirmation": "any_password"
+    }
+    request = HttpRequest(body=attributes)
+    httpResponse = sut.route(request)
+    assert httpResponse.status_code == 400
+
+
+def test_should_return_200_if_valid_para_is_provided():
+    sut = SignUpController()
+    attributes = {
+        "name": faker.word(),
+        "login": faker.word(),
+        "email": "any_email@mail.com",
+        "email_confirmation": "any_email@mail.com",
+        "password": "any_password",
+        "password_confirmation": "any_password"
     }
     request = HttpRequest(body=attributes)
     httpResponse = sut.route(request)
